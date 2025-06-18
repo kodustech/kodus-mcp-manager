a12de6c3b880b8ac5600dccdac881db9141b50d9
Italo Peruchi <italo.peruchi@newtail.com.br>
2025-06-17 22:01:28 -0300

    Add class-transformer and improve tests

--- src/modules/composio/dto/query.dto.ts
+++ src/modules/composio/dto/query.dto.ts
@@ -1,18 +1,37 @@
-import { IsNumber, IsString } from 'class-validator';
+import { IsNumber, IsOptional, IsString, Min, Max } from 'class-validator';
+import { Type } from 'class-transformer';
 
 export class QueryDto {
   @IsNumber()
+  @IsOptional()
+  @Type(() => Number)
+  @Min(1, {
+    message: 'page must be a number conforming to the specified constraints',
+  })
   page = 1;
 
   @IsNumber()
+  @IsOptional()
+  @Type(() => Number)
+  @Min(1, {
+    message:
+      'pageSize must be a number conforming to the specified constraints',
+  })
+  @Max(100, {
+    message:
+      'pageSize must be a number conforming to the specified constraints',
+  })
   pageSize = 10;
 
   @IsString()
+  @IsOptional()
   integrationName: string;
 
   @IsString()
+  @IsOptional()
   integrationId: string;
 
   @IsString()
+  @IsOptional()
   entityId: string;
 }
